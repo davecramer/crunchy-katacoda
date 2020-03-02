c4bec98f5d414cd36b85b03a36620ad423b95cf4
Dave Cramer <davecramer@gmail.com>
2020-03-02 15:58:29 -0500

    added some more packages

--- workshops/plr/env-init.sh
+++ workshops/plr/env-init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 yum -y install plr11
 yum -y install gdal.x86_64 gdal-devel.x86_64 gdal-libs.x86_64
-yum -y install proj.x86_64 proj-devel.x86_64
+yum -y install proj.x86_64 proj-devel.x86_64 geos geos-devel
 
 systemctl enable postgresql-11
 systemctl start postgresql-11
