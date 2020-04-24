fb20b5aef51dd84bfcd811bdf927e1fbc54e54e3
Dave Cramer <davecramer@gmail.com>
2020-04-24 11:51:22 -0400

    sudo to postgres not training

--- workshops/replication/set-env.sh
+++ workshops/replication/set-env.sh
@@ -5,7 +5,7 @@
 CURRENT_USER=$(whoami)
 
 until [ $CURRENT_USER == "postgres" ]; do
-    sudo -iu training
+    sudo -iu postgres
     CURRENT_USER=$(whoami)
     echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for current user to change to postgres..."
     sleep 1;
